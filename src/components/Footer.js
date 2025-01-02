5f21b1824bbf108792e48e632e7e12c48a79af9e
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-02 22:39:29 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './Footer.css'; // Import the Footer CSS fil
+import React from 'react';
+import './Footer.css'; // Import the Footer CSS fil
 
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} TV Shows App. All rights reserved.</p>
+        <p>
+          Designed and developed by <a href="https://adityapalodkarportfolio.netlify.app/">Aditya Palodkar</a>.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
 const Footer = () => {
   return (
     <footer className="footer">
