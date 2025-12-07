32339c727ec07e6383b22f8a8d17d18a00e42a65
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-12-07 19:12:40 +0530

    Update ShowList.js

--- src/components/ShowList.js
+++ src/components/ShowList.js
@@ -1,4 +1,4 @@
-1import React, { useState, useEffect } from 'react';
+21import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './ShowList.css'; // Import the CSS file
